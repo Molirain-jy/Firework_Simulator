c497beec16a5c62e182b5ebf8e1b3958d51e74a6
Molirain-jy <starry-sky.hjy@qq.com>
2025-05-20 12:11:56 +0800

    Update script.js
    更改文字

--- js/script.js
+++ js/script.js
@@ -78,7 +78,7 @@ const mainStage = new Stage("main-canvas");
 const stages = [trailsStage, mainStage];
 
 //随机文字烟花内容
-const randomWords = ["新年快乐", "心想事成"];
+const randomWords = ["愿保兹善","千载为常","欢笑尽娱","乐哉未央"];
 const wordDotsMap = {};
 randomWords.forEach((word) => {
 	wordDotsMap[word] = MyMath.literalLattice(word, 3, "Gabriola,华文琥珀", "90px");
